9c2eda0555efdc7ed1f79acc4fb2732e55d5c71c
cindraayadathil <cindraayadathil@gmail.com>
2024-05-21 12:05:59 +0530

    authenticated the login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import logo from '../Components/Footer/giphy.gif'
+import axios from 'axios'
+
 function Login() {
-  return (
+  const [phonenumber,setPhonenumber] = useState();
+
+  const navigate = useNavigate()
+
+  const Handlechange=(x)=>{
+// console.log(x.target.value)
+setPhonenumber(x.target.value);
+  }
+
+  // const  Handlesubmit=(y) =>{
+  //   y.preventDefault()
+  //   //request to server
+  // axios.post('https://hak.pythonanywhere.com/auth/customer/',{phone_number:phonenumber}).then((response)=>console.log(response))
+
+
+  const Handlesubmit = async (y) => {
+    y.preventDefault();
+    try {
+      const response = await axios.post('https://hak.pythonanywhere.com/auth/customer/', { phone_number: phonenumber });
+      if (response.status === 201) {
+        navigate('/send-otp');
+      } else {
+        console.log('Unexpected response status:', response.status);
+      }
+    } catch (error) {
+      console.error('There was an error!', error);
+    }
+   console.log(y)
+   
+  }
+    return (
     <div className='login'>
        <div className="contr">
       <div className="left">
@@ -16,13 +48,16 @@ function Login() {
           </header>
           <form className='frm-lg'>
             <input
+            name='phone_number'
               type="text"
               placeholder="Enter Your Mobile Number"
               className="input-field animation a3"
+              onChange={Handlechange}
               maxLength={10}
               required
             />
-            <Link to="/send-otp" className="animation a4" ><button className="btlg">Send OTP</button></Link>
+            {/* <Link to="/send-otp" className="animation a4" ></Link> */}
+            <button className="btlg" onClick={Handlesubmit}>Send OTP</button>
           </form>
         </div>
       </div>
